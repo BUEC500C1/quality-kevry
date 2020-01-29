f43c333f42db469f37f9703c54093d048729703d
Kevin Delgado <thekevry@gmail.com>
2020-01-29 10:11:43 -0500

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -6,3 +6,17 @@ def test_func1():
 	assert conversion_func(4) == "IV"
 def test_func2():
 	assert conversion_func(5) == "V"
+def test_func3():
+	assert conversion_func(310) == "CCCX"
+def test_func4():
+	assert conversion_func(17) == "XVII"
+def test_func5():
+	assert conversion_func(500) == "D"
+def test_func6():
+	assert conversion_func(137) == "CXXXVII"
+def test_func7():
+	assert conversion_func(1998) == "MCMXCVIII"
+def test_func8():
+	assert conversion_func(270) == "CCLXX"
+def test_func9():
+	assert conversion_func(46) == "XLVI"

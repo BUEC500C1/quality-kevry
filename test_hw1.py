e7055e07b829f7b3921dee0b124d40b0354d2566
Kevin Delgado <thekevry@gmail.com>
2020-01-29 10:06:06 -0500

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -2,9 +2,7 @@ from hw1 import conversion_func
 import pytest
 
 
-def test_func():
+def test_func1():
 	assert conversion_func(4) == "IV"
-  
+def test_func2():
 	assert conversion_func(5) == "V"
-  
-	assert conversion_func(9) == "IX"
